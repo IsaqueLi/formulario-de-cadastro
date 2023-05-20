4296ba65a705449561222948fd8230c0ec6c1346
IsaqueLi <isakilica16@gmail.com>
2023-05-20 16:28:00 -0300

    implementando nova funcao para tratar erros de cep invalido

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-	.then(resposta => resposta.json())
-	.then(r => {
-		if(r.erro) {
-			throw Error('Esse cep não existe!')
-		}else {
-			console.log(r) 
-		}	
-	})
-	.catch(erro => console.log(erro))
-	.finally(mensagem => console.log('processamento concluído!'))
+async function buscaEndereco() {
+	try{
+		var consultaCEP = await fetch('https://viacep.com.br/ws/01001250/json/')
+		var consultaCEPConvertida = await consultaCEP.json();
+		if(consultaCEPConvertida.erro){
+			throw Error('CEP não existente!')
+		}
+	}catch(erro){
+		console.log(erro)
+	}
+}
 
-console.log(consultaCEP);
+buscaEndereco()
